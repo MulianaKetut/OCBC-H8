38c9692ca92dc6d009ca9d923004020573123054
muliana_ketut <ketutmuliana7@gmail.com>
2021-10-18 13:09:31 +0700

    update jwt with refresh token and expired

---
+++ SESI11/UpdateJWT/TodoApp/Configurations/JwtConfig.cs
@@ -0,0 +1,7 @@
+namespace TodoApp.Configurations
+{
+    public class JwtConfig
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ SESI11/UpdateJWT/TodoApp/Models/DTOs/Responses/ResponseMessage.cs
@@ -0,0 +1,9 @@
+namespace TodoApp.Models.DTOs.Responses
+{
+    public class ResponseMessage
+    {
+        public string Status { get; set; }
+
+        public string Message { get; set; }
+    }
+}
